fd687f6134e8635c0f5168091d00d1657e57c71e
Michelle Brownlee <michelle@orbwave.com>
2020-12-31 10:28:00 -0600

    replaced special characters

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <html>
-<head>
-    <title></title>
-</head>
+
 <body>
     <form method="post">
         <button type=submit name=submit class="btn">Grab it!</button>
     </form>
-<?php
-$getFile = "https://jobboards.phenommarket.com/feeds/oninglobal-en-global-feed-indeed";
-$setFile = "/Users/mbrownlee/php/xmlscrape/fileContents.txt";
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-$xml = file_get_contents($getFile); 
-file_put_contents($setFile, $xml); 
-echo "Success";
-}
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $getFile = "https://jobboards.phenommarket.com/feeds/oninglobal-en-global-feed-indeed";
+        $find = "&nbsp";
+        $place = " ";
+        $setFile = "/Users/mbrownlee/php/xmlscrape/fileContents.txt";
+        $xml = file_get_contents($getFile);
+        $txtFile = str_replace($find, $place, $xml);
+       
+        file_put_contents($setFile, $txtFile);
+        echo "Success";
+    }
 
-?>
+    ?>
 </body>
 <style>
     .btn {
         background-color: purple;
         color: white;
     }
-    </style>
+</style>
+
 </html>
